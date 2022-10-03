0f64c06a640cc9000893d5ee7a4ea6b7fd1e3f9d
Trisha <Trisha>
2022-10-03 14:20:30 -0400

    Created list of words and corresponding definitions --> returns random word from list and its definition when we go to page /words

--- api_test.py
+++ api_test.py
@@ -1,8 +1,13 @@
 from flask import Flask, jsonify
 from threading import Thread
+import numpy as np
+
 
 
 app = Flask('')
+
+words = ["blasphemy", "monstrous", "gargantuan", "cyclone"]
+definitions = ["the act or offense of speaking sacrilegiously about God or sacred things; profane talk.", "having the ugly or frightening appearance of a monster.", "enormous.", "a system of winds rotating inward to an area of low atmospheric pressure, with a counterclockwise (northern hemisphere) or clockwise (southern hemisphere) circulation; a depression." ]
 
 
 @app.route('/')
@@ -13,6 +18,13 @@ def home():
 @app.route('/testadd', methods=['GET'])
 def add():
   return jsonify({"2 + 2": 2 + 2})
+
+@app.route('/words', methods = ['GET'])
+def retWord():
+  index = np.random.randint(0, len(words)-1)
+  word = words[index]
+  return jsonify(word, definitions[index])
+
 
 
 def run():
