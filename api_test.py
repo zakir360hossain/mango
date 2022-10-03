3dfb324ae45606d0101ccb022bfd2a64c422fb0f
Trisha <Trisha>
2022-10-03 14:46:58 -0400

    Added some more to the api. Still learning :)

--- api_test.py
+++ api_test.py
@@ -8,27 +8,39 @@ app = Flask('')
 
 words = ["blasphemy", "monstrous", "gargantuan", "cyclone"]
 definitions = ["the act or offense of speaking sacrilegiously about God or sacred things; profane talk.", "having the ugly or frightening appearance of a monster.", "enormous.", "a system of winds rotating inward to an area of low atmospheric pressure, with a counterclockwise (northern hemisphere) or clockwise (southern hemisphere) circulation; a depression." ]
-
+currentIndex = 0
 
 @app.route('/')
 def home():
   return "I'm alive"
 
 
-@app.route('/testadd', methods=['GET'])
+#uses GET by default
+@app.route('/testadd')
 def add():
   return jsonify({"2 + 2": 2 + 2})
 
-@app.route('/words', methods = ['GET'])
-def retWord():
+@app.route('/clue')
+def retClue():
   index = np.random.randint(0, len(words)-1)
-  word = words[index]
-  return jsonify(word, definitions[index])
+  currentIndex = index
+  clue = definitions[index]
+  return jsonify(definitions[index])
+
+@app.route('/answer')
+def retAnswer():
+  return jsonify(words[currentIndex])
+
+
 
 
 
 def run():
-  app.run(host='0.0.0.0', port=7000)
+  #app.run(host='0.0.0.0', port=7000)
+  app.run()
+
+if __name__ == '__main__':
+  app.run()
 
 
 t = Thread(target=run)
