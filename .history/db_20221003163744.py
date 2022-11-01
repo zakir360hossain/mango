bd0dad3d3d1a729bdd4c2ac3f91e23e6e3d45b80
Zakir Hossain <zakirhossain@dhcp-130-58-103-187.swarthmore.edu>
2022-11-01 19:53:24 -0400

    setup flask

--- .history/db_20221003163744.py
+++
@@ -1,9 +0,0 @@
-# import json
-import pymongo
-
-
-connection_url = "mongodb+srv://zakir:mongo123@cluster0.abqkq.mongodb.net/?retryWrites=true&w=majority"
-client = pymongo.MongoClient(connection_url)
-
-pool = client.get_database("pool")
-
---
+++ app/views.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit, send
+import pymongo
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "mango"
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+numberOfClients = 0
+
+connection_url = "mongodb+srv://zakir:mongo123@cluster0.abqkq.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(connection_url)  # returns a client
+
+
+@app.route("/")
+def home():
+    return "Mango!!!!"
+
+
+@app.route("/category/<category>", methods=["GET"])
+def getCategoryData(category):
+    pool = client.get_database("pool")
+    mango_c = pool.mango  # mango collection
+    query = mango_c.find_one({"category": category})
+    return {"category": query["category"], "objects": query["objects"]}
+
+
+@app.route("/home.html")
+def get_home():
+    return render_template("home.html")
+
+
+@socketio.event
+def arrive(msg):
+    print(f"\n{msg}\n")
+    emit("received", {"data": "next clue!"})
+
+
+@socketio.on("connect")
+def connect(msg):
+    global numberOfClients
+    numberOfClients += 1
+    emit('register', {"data": f"\nNumber of current clients: {numberOfClients}\n"})
+
+
+@socketio.on("message")
+def message(msg):
+    print(msg)
+    emit("message", {"data": "welcome!"})
+
+
+if __name__ == "__main__":
+    socketio.run(app, allow_unsafe_werkzeug=True)
+    client.close()
